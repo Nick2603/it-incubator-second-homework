e5fadbc312331450d5ce04d2f19d15ba7e0707a3
MYKYTA VOSTRIKOV <mykytavostrikov@gmail.com>
2023-03-06 20:55:28 +0200

    fixed auth

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -2,13 +2,14 @@ import { NextFunction, Request, Response } from "express";
 import { CodeResponsesEnum } from "../types/CodeResponsesEnum";
 
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  const authType = (req.headers.authorization || '').split(' ')[0];
   const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
   const strauth = Buffer.from(b64auth, 'base64').toString();
   const splitIndex = strauth.indexOf(':');
   const login = strauth.substring(0, splitIndex);
   const password = strauth.substring(splitIndex + 1);
 
-  if (login === "admin" && password === "qwerty") {
+  if (authType === "Basic" && login === "admin" && password === "qwerty") {
     next();
   } else {
     res.status(CodeResponsesEnum.Unauthorized_401).send({ message: "Unauthorized" });
